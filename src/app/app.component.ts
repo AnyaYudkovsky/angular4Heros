e217cd3b92e827b26f0694f4665156dc88ff6cad
Anna Yudkovsky <anna.yudkovsky@zoomdata.com>
2017-06-08 11:10:14 -0400

    Step 3 - Multiple Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,18 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 
 @Component({
   selector: 'my-app',
@@ -10,14 +24,7 @@ import { Component } from '@angular/core';
       <span class="badge">{{hero.id}}</span> {{hero.name}}
     </li>
   </ul>
-  <div *ngIf="selectedHero">
-  <h2>{{selectedHero.name}} details!</h2>
-  <div><label>id: </label>{{selectedHero.id}}</div>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="selectedHero.name" placeholder="name">
-  </div>
-  </div>`,
+  <hero-detail [hero]="selectedHero"></hero-detail>`,
   styles: [`
   .selected {
     background-color: #CFD8DC !important;
@@ -77,22 +84,5 @@ export class AppComponent  {
   };
 }
 
-export class Hero {
-  id: number;
-  name: string;
-}
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
 
 
